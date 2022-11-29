4987ee2e45bc487bb5b115a6b9c921062759fe13
EncouragingGunnar <bsing7896@gmail.com>
2022-11-29 10:59:30 +0100

    Tilemap fixed?

--- Chicken Runners/Scenes/Knights.gd
+++ Chicken Runners/Scenes/Knights.gd
@@ -10,13 +10,31 @@ var mouse_pos = Vector2.ZERO
 var can_generate_path = true
 var path = get_node("MapNavigation").get("new_path")
 
+
 func _ready() -> void:
 	global_position = Vector2(300, 300)
+	set_process(false)
 
 		
 func _process(delta: float) -> void:
 	if can_generate_path:
 		pass
 
-
-
+func move_along_path(distance : float) -> void:
+	var start_point = position
+	for i in range(path.size()):
+		var distance_to_next = start_point.distance_to(path[0])
+		if distance <= distance_to_next and distance >= 0.0:
+			position = start_point.linear.interpolate(path[0], distance / distance_to_next)
+			break
+		elif distance < 0.0:
+			position = path[0]
+		distance -= distance_to_next
+		start_point = path[0]
+		path.remove(0)
+		
+func set_path(value: PoolVector2Array) -> void:
+	path = value
+	if value.size() == 0:
+		return
+	set_process(true)
--- Chicken Runners/Scenes/Level.gd
+++ Chicken Runners/Scenes/Level.gd
@@ -3,10 +3,17 @@ extends Node2D
 onready var mapnavigation = $MapNavigation
 onready var knight = $Knights
 
+
 func _ready() -> void:
 	pass
 
-var new_path = mapnavigation.get_simple_path(knight.position,get_global_mouse_position())	
-
+func _unhandled_input(event: InputEvent) -> void:
+	if not event is InputEventMouseButton:
+		return
+	if event.button_index != BUTTON_LEFT or not event.pressed:
+		return
 	
+	var new_path = mapnavigation.get_simple_path(knight.position(), get_global_mouse_position())	
+	knight.position = new_path
+
 
