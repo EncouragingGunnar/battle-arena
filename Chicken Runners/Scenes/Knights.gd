34dc6d343111f32540c49e4969cd6d8a72b64f1e
EncouragingGunnar <bsing7896@gmail.com>
2022-12-06 10:59:06 +0100

    Navigation added

--- Chicken Runners/Scenes/Knights.gd
+++ Chicken Runners/Scenes/Knights.gd
@@ -8,16 +8,22 @@ var health := 300
 var dps := 50
 var mouse_pos = Vector2.ZERO
 var can_generate_path = true
-onready var _agent = $NavigationAgent2D
-onready var knight = $Knights
+onready var agent = $NavigationAgent2D
+
 
 func _ready() -> void:
-	global_position = Vector2(300, 300)
-	set_process(false)
+	pass
+	
+func _physics_process(delta: float) -> void:
+	_move_knight(delta)
 
+func _move_knight(delta: float) -> void:
+	agent.set_target_location(get_global_mouse_position())
+	if Input.is_action_pressed("use"):
+		var direction := global_position.direction_to(agent.get_next_location())
+		var desired_velocity = direction * max_velocity
+		velocity = velocity.move_toward(desired_velocity, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, ACCELERATION * delta)
 		
-func _process(delta: float) -> void:
-	if can_generate_path:
-		pass
-
-
+	move_and_collide(velocity * delta)
