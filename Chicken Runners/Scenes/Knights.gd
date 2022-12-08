f6275d86286a66f047413f0a45e4594fef940505
EncouragingGunnar <bsing7896@gmail.com>
2022-12-08 10:58:10 +0100

    navigation experiments

--- Chicken Runners/Scenes/Knights.gd
+++ Chicken Runners/Scenes/Knights.gd
@@ -1,29 +1,31 @@
 extends KinematicBody2D
 
-const ACCELERATION = 1000
-
-var max_velocity := 500
 var velocity := Vector2.ZERO
 var health := 300
 var dps := 50
 var mouse_pos = Vector2.ZERO
-var can_generate_path = true
 onready var agent = $NavigationAgent2D
+var has_arrived = true
 
 
 func _ready() -> void:
 	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		has_arrived = false			
+		var target_location = get_global_mouse_position()
+		agent.set_target_location(target_location)
+	
 	
 func _physics_process(delta: float) -> void:
-	_move_knight(delta)
+	var move_direction = position.direction_to(agent.get_next_location())
+	velocity = move_direction * agent.max_speed
+	agent.set_velocity(velocity)
+	
+	if not has_arrived:
+		move_and_slide(velocity)
+	if agent.is_navigation_finished():
+		has_arrived = true
 
-func _move_knight(delta: float) -> void:
-	agent.set_target_location(get_global_mouse_position())
-	if Input.is_action_pressed("use"):
-		var direction := global_position.direction_to(agent.get_next_location())
-		var desired_velocity = direction * max_velocity
-		velocity = velocity.move_toward(desired_velocity, ACCELERATION * delta)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, ACCELERATION * delta)
-		
-	move_and_collide(velocity * delta)
+	
